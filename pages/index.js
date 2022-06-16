0a7b192478d040ff2f24aff7080f749f4f375c68
Yassar021 <yassar.tkj114@gmail.com>
2022-06-16 13:35:18 +0700

    addHead

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,22 @@
 import { Box } from "@chakra-ui/react"
+import Head from "next/head"
 import Footer from "../components/Footer"
 import Home from "../components/Home"
 import Navbar from "../components/Navbar"
 
 const Index = () => {
   return (
+    <>
+    <Head>
+      <title>Yassar</title>
+    </Head>
+
     <Box bgColor={'#02091C'}>
         <Navbar />
         <Home />
         <Footer />
     </Box>
+    </>
   )
 }
 
